a3db7f62bfd69743fcad4db062748468e62e08eb
Max IMBERT <maximbert@gmail.com>
2018-08-03 00:10:49 -0700

    Removed CORS (unnecessary since now we're running one server instead of two on separate ports)

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const mergeImg = require('merge-img');
-const path = require('path');
 
 const app = express();
 
-//const cors = require('cors');
-//app.use(cors({origin: 'http://localhost:8000'}));
+// the root endpoint / serves index.html
+app.use(express.static(__dirname + '/www'));
+
+// bower dependency (Dropzone) needs to be accessible too
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use(fileUpload());
 
-app.use(express.static(__dirname + '/www'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-
+// upload endpoint: takes multiples image files, combines them and returns a dataURI
 app.post('/upload', function(req, res) {
   if (!req.files || req.files.length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
-
   const files = Object.values(req.files).map(({data}) => data);
   mergeImg(files)
   .then((img) => {
@@ -28,4 +27,4 @@ app.post('/upload', function(req, res) {
   });
 });
 
-app.listen(3000, () => console.log("Backend upload service started on port 3000"));
+app.listen(3000, () => console.log("Server started on port 3000"));
